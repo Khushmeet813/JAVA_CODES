df866c4b909b91da7b728ba6f445bcdcae3a042c
Khushmeet Kaur <khushmeet813@gmail.com>
2026-07-18 21:57:30 +0530

    Add files via upload

---
+++ Day_1/LabReadinessChecker.java
@@ -0,0 +1,26 @@
+package Day_1;
+
+public class LabReadinessChecker {
+
+	public static void main(String[] args) {
+		
+		// Practical 1: Java Environment Readiness Checker
+		
+
+		String studentName = "Khushmeet Kaur";
+		String laboratoryName = "Java Programming Lab";
+
+		System.out.println("JAVA LAB READINESS CHECK");
+
+		System.out.println("Student Name : " + studentName);
+		System.out.println("Laboratory : " + laboratoryName);
+		System.out.println("Status : Java environment is ready.");
+		System.out.println("Compile : javac LabReadinessChecker.java");
+		System.out.println("Run : java LabReadinessChecker");
+		
+		
+		}
+
+		}
+
+
